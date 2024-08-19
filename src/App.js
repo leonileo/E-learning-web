738e80c001b465b978764ea01ac62ec161907ae1
leonileo <leolittleprogrammer@gmail.com>
2024-08-19 03:26:04 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Banner from './components/Banner'
+import Subscribe from './components/Subscribe'
+import Banner2 from './components/Banner2'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className='overflox-x-hidden bg-white text-dark '>
-      <Navbar />
+      <Hero />
+      <Services />
+      <Banner />
+      <Subscribe />
+      <Banner2 />
+      <Footer />
     </div>
   )
 }
